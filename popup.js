165bcb3e58f831da6357f13f647ab318477fc5eb
John Mertens <john.mertens@gmail.com>
2012-12-03 12:42:37 -0800

    Code cleanup

--- popup.js
+++ popup.js
@@ -5,13 +5,13 @@ var candidate_search = function(possibles) {
   request.open('GET', url, true);
   console.log(url);
   request.onreadystatechange = function(ev) {
-    var i = 0;
+    var i = 0, candidates, results;
     if (request.readyState == 4) {
       if (request.status == 200) {
         console.log(request.responseText);
-        var candidates = JSON.parse(request.responseText);
+        candidates = JSON.parse(request.responseText);
         //callback(candidates);
-        var results = document.getElementById("results");
+        results = document.getElementById("results");
         results.innerHTML = '';
         for(; i < candidates.length; i+=1) {
           results.innerHTML += '<p>'+candidates[i].name+' '+candidates[i].title+'</p>';
@@ -29,8 +29,6 @@ var candidate_search = function(possibles) {
 
 function scan() {
   var possibles = chrome.extension.getBackgroundPage().selected_possibles;
-  console.log('LOOOK OVER HERE');
-  
   if (possibles) candidate_search(possibles);
 }
 window.onload = scan;
