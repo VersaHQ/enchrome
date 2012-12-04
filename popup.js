76383556944c5825807f4a08b1c5904e7d52fa08
John Mertens <john.mertens@gmail.com>
2012-12-04 15:49:11 -0800

    Move candidate search callback into its own method

--- popup.js
+++ popup.js
@@ -1,34 +1,40 @@
-var candidate_search = function(possibles) {
-  var url = 'http://electnext.dev/api/v1/candidate_search.js?possibles[]=' + possibles.join('&possibles[]=');
+var candidate_search = function(possibles, callback) {
+  var url = 'http://electnext.com/api/v1/candidate_search.js?possibles[]=' + possibles.join('&possibles[]=');
   var request = new XMLHttpRequest();
 
   request.open('GET', url, true);
   console.log(url);
   request.onreadystatechange = function(ev) {
-    var i = 0, candidates, results;
+    var candidates;
     if (request.readyState == 4) {
       if (request.status == 200) {
         console.log(request.responseText);
         candidates = JSON.parse(request.responseText);
-        //callback(candidates);
-        results = document.getElementById("results");
-        results.innerHTML = '';
-        for(; i < candidates.length; i+=1) {
-          results.innerHTML += '<p>'+candidates[i].name+' '+candidates[i].title+'</p>';
-        }
+        callback(candidates);
       } else {
-        results.innerHTML = '<p>Couldn\'t find politicians...</p>';
-        // callback(null);
+        callback(null);
       }
     }
   };
   request.send(null);
 };
 
+var display_results = function(candidates) {
+  var i = 0, results;
+  results = document.getElementById("results");
+  results.innerHTML = '';
+  if(candidates) {
+    for(; i < candidates.length; i+=1) {
+      results.innerHTML += '<p>'+candidates[i].name+' '+(candidates[i].title || '')+'</p>';
+    }
+  } else {
+    results.innerHTML = '<p>Couldn\'t find politicians...</p>';
+  }
+};
 
 
 function scan() {
   var possibles = chrome.extension.getBackgroundPage().selected_possibles;
-  if (possibles) candidate_search(possibles);
+  if (possibles) candidate_search(possibles, display_results);
 }
 window.onload = scan;
